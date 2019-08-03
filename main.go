ed35935e2404daf7d3076c20574923119e47bbbb
Leonardo F <leonardof@tutanota.com>
2019-08-03 17:48:10 -0300

    v0.1: opção para verificar se está aberto ou não

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"log"
-	"marmitaz-telegram-bot/site"
-	"marmitaz-telegram-bot/telegram"
 	"os"
 
-	"github.com/robfig/cron"
+	"github.com/Valeyard1/marmitaz-telegram-bot/site"
 	"github.com/yanzay/tbot/v2"
 )
 
@@ -19,20 +17,44 @@ var (
 func main() {
 	bot := tbot.New(token, tbot.WithLogger(tbot.BasicLogger{}))
 	client = bot.Client()
+	if chatID == "" || token == "" {
+		log.Fatal("chatID or token not passed")
+	}
 
-	c := cron.New()
-	c.AddFunc("30 * * * * *", func() {
-		if site.TemperoDeMaeIsOpen() {
-			client.SendMessage(chatID, "Open")
-		} else {
-			client.SendMessage(chatID, "Closed")
-		}
-	})
-	c.Start()
+	log.Println(chatID, token)
 
-	log.Println(c.Entries())
+	//c := cron.New()
+	//c.AddFunc("30 * * * * *", func() {
+	//	if site.TemperoDeMaeIsOpen() {
+	//		client.SendMessage(chatID, "Open")
+	//	} else {
+	//		client.SendMessage(chatID, "Closed")
+	//	}
+	//})
+	//c.Start()
 
-	bot.HandleMessage("/status", telegram.StatusHandler)
-	bot.HandleMessage("/help", telegram.HelpHandler)
-	bot.Start()
+	bot.HandleMessage("/start", startHandler)
+	bot.HandleMessage("/status", statusHandler)
+	bot.HandleMessage("/help", helpHandler)
+	err := bot.Start()
+	log.Println(err)
+}
+
+// StatusHandler handle /status incoming messages
+func statusHandler(message *tbot.Message) {
+	if site.TemperoDeMaeIsOpen() {
+		client.SendMessage(message.Chat.ID, "O restaurante está aberto. Faça seu pedido")
+	} else {
+		client.SendMessage(message.Chat.ID, "O restaurante está fechado.")
+	}
+}
+
+// HelpHandler handle /help incoming messages
+func helpHandler(message *tbot.Message) {
+	client.SendMessage(message.Chat.ID, "Digite /status")
+}
+
+// StartHandler handle /start incoming messages
+func startHandler(message *tbot.Message) {
+	client.SendMessage(message.Chat.ID, "Sou uma interface para o site de restaurantes marmitaz.pushsistemas.com.br\nPara mais informações digite /help")
 }
